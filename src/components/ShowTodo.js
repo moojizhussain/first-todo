a724aebb5a1cbcfb356df9ddf4e7ac08cdcddf94
Moojiz <moojizhussain5@gmail.com>
2022-04-02 22:07:02 +0500

    first commit

---
+++ src/components/ShowTodo.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ShowTodo(props) {
+  return (
+    <div className="container">
+      <div className="row my-3">
+        <div className="col-6">
+          <h6>{props.task}</h6>
+        </div>
+        <div className="col-6">
+          <button
+            onClick={() => {
+              props.onSelcet(props.id);
+            }}
+          >
+            X
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ShowTodo;
